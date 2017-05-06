909d0e7f472fd7569a1da91b36acf8d113222b13
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-05-06 04:55:23 -0400

    mvc patter 1st sprint

--- register.php
+++ register.php
@@ -1,23 +1,41 @@
-<html>
-<body>
-  <h1>REGISTER</h1>
-  <form method = 'post' action = 'index.php'>
-  <strong> Username: </strong> 
-  <input type ='text' name= 'reg_uname' value="" /> </ br></ br> <br><br>
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Register</title>
   
-  <strong> EMAIL </strong> 
-  <input type ='text' name= 'reg_email' value="" /> </ br></ br> <br><br>
-  
-  <strong> PASSWORD </strong> 
-  <input type ='password' name= 'reg_password' value="" /> </ br></ br>
-  <input type ='hidden' name='action' value='register'> <br><br>
-  <input type='submit' value = "Register">  
-  </form>
-  
-  <form method = 'post' action = 'login.php'>
-  <input type = 'submit' value ='try to login' />
-  </form>
+    <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
 
+<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+<div class = "title"><h6>TodoList - Smart Task List Manager</h6></div>
+  
+<form method="post" action="index.php">
+<div class="form">
+  <div class="fColor"></div>
+  <div class="tbar">
+    <div class="sColor"></div>
+    <input type="Username" class="input" name="reg_uname" placeholder="Username"/>
+    <input type="Email" class="input" name="reg_email" placeholder="Email"/>
+    <input type="password" class="input" name="reg_password" placeholder="Password"/>
+    
+    <input type="submit" class = "submit" value="register" />
+</div>
+
+</form>
+<form method = 'post' action = 'login.php'>
+<div class= "rig"><p>If You already Have an account:</p>
+<input type = 'submit' class= "submit" value ='Login' />
+</div>
+</form>  
+</div>
 
 </body>
 </html>
