6abe508e02ffc022901bc7e2b8b84861375bf06d
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-05-06 20:15:16 -0400

    updated form and header

--- login.php
+++ login.php
@@ -1,39 +1,31 @@
 <!DOCTYPE html>
 <html >
 <head>
-  <meta charset="UTF-8">
-  <title>TodoList</title>
-  
-
-<link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
-
-<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
-  
-<link rel="stylesheet" href="css/style.css">
-  
+<meta charset="UTF-8">
+  <title>TO DO LIST</title>
+	<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+	<link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css'>
+	<link rel="stylesheet" href="css/style.css">	  
 </head>
 
 <body>
-<div class = "title"><h6>TodoList - Smart Task List Manager</h6></div>
-  
-<form method = "post" action="index.php" class="login">
-<div class="form">
-  <div class="fColor"></div>
-  <div class="tbar">
-    <div class="sColor"></div>
-    <input type="Username" class="input" name="reg_uname" placeholder="Username"/>
-    <input type="password" class="input" name="reg_password" placeholder="Password"/>
-  </div>
-  <button class="submit" name="submit" >Login</button>
-  <input type ="hidden" name="action" value="test_user">
+	<div class = "title"><h1>TodoList - Smart Task List Manager</h1></div>
+  <div class="wrapper">
   <br>
-  
-</form>
+  <div class="form-signin">
+  	<h2 class="form-signin-heading">Login</h2>
+	<form method="post" action="./Todo-items-manager/index.php">
+        <input type="text" class="form-control" placeholder="E-mail" name="email"><br>
+	<input type="password" class="form-control" placeholder="password" name="password"><br>
+	<input type ="hidden" name="action" value="test_user">
+        <input type="submit" class="btn btn-lg btn-primary btn-block" value="Login" name="submit">
+	</form>
 
-  <form action = "register.php" class= "register">
-  <button class="submit" name="submit" >SignUp</button>
-  </form>
+	<br><br>
+	<form class="form-login" method="post" action="register.php">
+	<input type="submit" class="btn btn-lg btn-primary btn-block" value="Sign-Up" name="submit">
+	</form>
 </div>
-  
+
 </body>
 </html>
