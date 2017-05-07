9bd7367ef182edeaef228eca5ee19d7056657181
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-05-06 21:33:20 -0400

    updated form and header

--- register.php
+++ register.php
@@ -2,40 +2,30 @@
 <html >
 <head>
   <meta charset="UTF-8">
-  <title>Register</title>
-  
-    <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
-
-<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
-
-  
-      <link rel="stylesheet" href="css/style.css">
-
-  
+  <title>TO DO LIST</title>
+ <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+	<link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css'>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-<div class = "title"><h6>TodoList - Smart Task List Manager</h6></div>
+  <div class = "title"><h1>TodoList - Smart Task List Manager</h1></div>
+  <div class="wrapper">
+  <br>
+  <div class="form-signin">
+  	<h2 class="form-signin-heading">SIGN UP</h2>
+		<form method="post" action="./Todo-items-manager/index.php">
+		        <input type="text" class="form-control" placeholder="First Name" name="firstname"><br>
+				<input type="text" class="form-control" placeholder="Last Name" name="lastname"><br>
+				<input type="text" class="form-control" placeholder="Contact No" name="contact"><br>
+				<input type="text" class="form-control" placeholder="E-mail" name="mailid"><br>
+				<input type="text" class="form-control" placeholder="username" name="user"><br>
+				<input type="password" class="form-control" placeholder="password" name="password"><br>
+				<input type ="hidden" name="action" value="register">
+				<input type="submit" class="btn btn-lg btn-primary btn-block" value="Sign-Up">
+		</form>
+		</div>
+
   
-<form method="post" action="index.php">
-<div class="form">
-  <div class="fColor"></div>
-  <div class="tbar">
-    <div class="sColor"></div>
-    <input type="Username" class="input" name="reg_uname" placeholder="Username"/>
-    <input type="Email" class="input" name="reg_email" placeholder="Email"/>
-    <input type="password" class="input" name="reg_password" placeholder="Password"/>
-    
-    <input type="submit" class = "submit" value="register" />
-</div>
-
-</form>
-<form method = 'post' action = 'login.php'>
-<div class= "rig"><p>If You already Have an account:</p>
-<input type = 'submit' class= "submit" value ='Login' />
-</div>
-</form>  
-</div>
-
 </body>
 </html>
