98316c1f0679f2e82bde6a014f2861255000ea7d
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-05-08 14:35:28 -0400

    edited badinfo page

--- error/badInfo.php
+++ error/badInfo.php
@@ -1,3 +1,7 @@
-<?php
-  echo "Wrong username or pass, try again!";
-  ?>
+<div id="main">
+<h1 class="top">Error</h1>
+<p class="first_paragraph">User name or password is incorrect</p>
+</div>
+<form method = 'post' action='../index.php'>
+<input type="submit" value="Login Page"/>
+</form>
