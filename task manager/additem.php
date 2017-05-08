1e8c0ffc8a4ce2e4aeb96756174cfb4f1f5dde9a
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-05-08 14:59:40 -0400

    added class header and edited form

--- task manager/additem.php
+++ task manager/additem.php
@@ -8,14 +8,24 @@
 </head>
 
 <body>
+  <div class ="header">
 	<div class = "title"><h1>TodoList - Smart Task List Manager</h1></div>
+  </div>
 <div class="form-style-2">
 <div class="form-style-2-heading">Provide information</div>
 <form class="add_task_box" method = 'post' action='index.php'>
-<label for="field1"><span>Task</span><input type='text' class="input-field" name='task'/><br>
-<label for="field2"><span>Description</span><input type='text' class="input-field" name='description'/><br>
-<label for="field2"><span>Date</span><input type="date" class="input-field" name = "datetodo"><br>
-<label for="field1"><span>Time</span><input type="time" class="input-field" name="timetodo"><br>
+<label for="field1"><span>Task</span>
+<input type='text' class="input-field" name='task'/></label>
+
+<label for="field2"><span>Description</span>
+<textarea class="field-long field-textarea" name='description'/></textarea></label>
+
+<label for="field2"><span>Date</span>
+<input type="date" class="input-field" name = "datetodo"></label>
+
+<label for="field1"><span>Time</span>
+<input type="time" class="input-field" name="timetodo"></label>
+
 <input type="hidden" name="userid" value="<?php echo $_COOKIE['my_id']; ?>">
 <input type = 'hidden' name = 'action' value='additem'><br>
 <label><span>&nbsp;</span><input type="submit" value="Add Task"/></label>
