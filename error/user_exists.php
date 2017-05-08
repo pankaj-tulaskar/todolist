e0cfd03b8eb952ced68e6b3b128373d152518b3f
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-05-08 14:39:48 -0400

    edited user exists

--- error/user_exists.php
+++ error/user_exists.php
@@ -1,11 +1,10 @@
 <?php
-  echo " the user exists, pls change a new name! </br>";
+  echo " <h1> User Name Already Exist</h1> </br>";
   ?>
   <html>
   <body>
-  <form action = "register.php">
-  
-  <input type="submit" value = "continue to register" />
+  <form method='post' action = '../register.php'>
+  <input type="submit" value = "Register Page" />
   </form>
   </body>
   </html>
