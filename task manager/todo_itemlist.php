fb22f78470b3028e59a8cf2ac70e5b3c578c9cb4
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-05-08 17:59:02 -0400

    added logout button

--- task manager/todo_itemlist.php
+++ task manager/todo_itemlist.php
@@ -1,24 +1,27 @@
-<html>
+
+<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
   <title>TO DO LIST</title>
   <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
   <link rel="stylesheet" href="../css/style_list.css" type="text/css">
-  <link rel="stylesheet" href="css/style.css">    
+      
 </head>
 <body>
   <div class ="header">
   <div class = "title"><h1>TodoList - Smart Task List Manager</h1></div>
   </div>
+  <br>
+  <form method = 'post' class ='button' action='../logout.php'><input type="submit" value="LOGOUT"/>
+    </form>
   <div class="welcome">
- 
-  <p> Welcome,<?php echo $_COOKIE['login']?></p>
+ <p>Welcome, <?php echo $_COOKIE['fname']?><span>&nbsp;</span><?php echo $_COOKIE['lname']?></p>
   </div>
    <h3><strong style="align:center">To-do Tasks</strong></h3>
-    <div style: "align:right">
+
     <form method = 'post' action='additem.php'>
-   <br> <input type="submit" value="Add a to-do"/>
-    </form></div><br>
+    <input type="submit" value="Add a to-do"/>
+    </form><br>
     <table>
        <tr>
       <th style="text-align: center;">Task</th>
@@ -41,14 +44,14 @@
         </td>
 
 <td>
-	   <form style="margin-top: 15px;" action="index.php" method= "post">
+	   <form style="margin-top: 14px;" action="index.php" method= "post">
      <input type="hidden" name= "user_id" value="<?php echo $res['id']; ?>">
      <input type="submit" style="text-align: center;" value = "Edit">
 	   <input type="hidden" name='action' value="edititem">
 	   </form>
 	</td>
  
-	<td><form style="margin-top: 15px;" action="index.php" method="post">
+	<td><form style="margin-top: 14px;" action="index.php" method="post">
             <input type="hidden" name="user_id" value="<?php echo $res['id']; ?>">
 	    <input style="text-align: center;" type="submit" value="Delete">
 	    <input type="hidden" name='action' value="delete_item">
@@ -59,7 +62,7 @@
       </tr>  
 	<?php endforeach;?>
       
-    </table>
+    </table><hr>
     <br><br>
       
  <h3><strong>Completed Tasks</strong></h3>
@@ -71,8 +74,8 @@
 	   </tr>
 	   <?php foreach($result2 as $res2):?>
 	   <tr>
-	   <td style="text-align: center; font-weight: bold;"> <?php echo $res2['todo']; ?>  </td>
-     <td style="text-align: center;"> <?php echo $res2['description']; ?>  </td>
+	   <td style="text-align: center; font-weight: bold;"> <?php echo $res2['todo']; ?></td>
+     <td style="text-align: center;"> <?php echo $res2['description']; ?></td>
 	   
            <td><form style="margin-top: 15px;" action="index.php" method="post">
                <input type="hidden" name="user_id" value="<?php echo $res2['id']; ?>">
@@ -82,7 +85,7 @@
 	  </td>
      </tr>
     <?php endforeach;?>
-    </table>
+    </table><hr>
 
   </body>
 </html>
