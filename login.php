c2713809bb368f963aee249729d4923153c6d6ed
pankaj s tulaskar <pst28@afsaccess1.njit.edu>
2017-05-08 22:44:14 -0400

    updated page style

--- login.php
+++ login.php
@@ -1,33 +1,39 @@
 <!DOCTYPE html>
-<html >
 <head>
 <meta charset="UTF-8">
   <title>TO DO LIST</title>
 	<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
-	<link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css'>
-	<link rel="stylesheet" href="css/style.css">	  
+  <link rel="stylesheet" href="css/style.css" type="text/css">	  
 </head>
 
 <body>
   <div class ="header">
 	<div class = "title"><h1>TodoList - Smart Task List Manager</h1></div>
   </div>
-  <div class="wrapper">
-  <br>
-  <div class="form-signin">
-  	<h2 class="form-signin-heading">Login</h2>
+  <div class="login">
+  <div class="heading">
+    <h2>Sign in</h2>
 	<form method="post" action="./task manager/index.php">
-        <input type="text" class="form-control" placeholder="E-mail" name="email"><br>
-	<input type="password" class="form-control" placeholder="password" name="password"><br>
+     <div class="input-group input-group-lg">
+        <span class="input-group-addon"><i class="fa fa-user"></i></span>   
+
+        <input type="text" class="form-control" placeholder="E-mail" name="email">
+      </div><br>
+
+     <div class="input-group input-group-lg">
+     <span class="input-group-addon"><i class="fa fa-lock"></i></span> 
+	<input type="password" class="form-control" placeholder="password" name="password">
+</div><br>
 	<input type ="hidden" name="action" value="test_user">
-        <input type="submit" class="btn btn-lg btn-primary btn-block" value="Login" name="submit">
+        <button type="submit" class="float" name="submit">LOGIN</button>
 	</form>
 
-	<br><br>
-	<form class="form-login" method="post" action="register.php">
-	<input type="submit" class="btn btn-lg btn-primary btn-block" value="Sign-Up" name="submit">
+	<br>
+ <h4>Click on Sign-up to create new account: </h4>
+	<form method="post" action="register.php">
+	<button type="submit" class="float" name="submit">Sign-up</button>
 	</form>
 </div>
-
+</div>
 </body>
 </html>
